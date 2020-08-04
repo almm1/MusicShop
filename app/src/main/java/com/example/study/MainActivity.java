78501d85d9c19135d1e7b2239daddd41b69bf51f
Иван Иванов <alatarev@yandex.ru>
2020-08-05 01:25:02 +0300

    add spinner add comments

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -3,18 +3,36 @@ package com.example.study;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     int quant = 0;//переменная хранящая в себе количесвто
     int price = 0; //цена
-    Spinner spinner;//спинер(меня с выпадающими элементами)
+    Spinner spinner;//спинер(меню с выпадающими элементами)
+    ArrayList spinnerBar;//массив для спиннера
+    ArrayAdapter spinnerAdapter;//связь массива со спинером
+    HashMap instruments;//словарь
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spinner = findViewById(R.id.spinner); //связь спиннера с переменной spinner
+        ArrayList spinnerBar = new ArrayList();//создание массива
+        spinnerBar.add("Guitar");
+        spinnerBar.add("Piano");
+        spinnerBar.add("Drum");
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerBar);//неведомая хрень
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);//установка выпадающего списка для спиннера
+        spinner.setAdapter(spinnerAdapter);//установка адаптера для спиннера
+
+        HashMap instruments = new HashMap();//создание словаря
+
+
     }
 
     public void quantityInc(View view) { //метод увеличения счетчика количества при нажатии кнопки плюс
